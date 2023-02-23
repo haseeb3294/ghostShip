b96b533f215fbc188256ce1d940c7fbc88cb517e
haseeb3294 <haseebraza293@gmail.com>
2023-02-23 19:37:19 +0500

    sidebar and header set same as figma

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,170 @@
+<div class="app-menu navbar-menu">
+    <!-- LOGO -->
+    <div class="navbar-brand-box">
+        <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover"
+                id="vertical-hover">
+            <i class="ri-record-circle-line"></i>
+        </button>
+    </div>
+
+    <div id="scrollbar">
+        <div class="container-fluid">
+            <div id="two-column-menu">
+            </div>
+            <ul class="navbar-nav" id="navbar-nav">
+                <div class="container-fluid">
+                    <div class="d-flex h-60 align-items-center justify-content-between border-bottom-1 px-3">
+                        <a href="javascript: void(0);" class="sidebar-icons">
+                            <img src="{{asset('assets/images/figma/icon.png')}}" height="15" width="15" alt="">
+                        </a>
+                        <form class="w-70">
+                            <input class="form-control form-control-sm border-0" type="text"
+                                   placeholder="How can I help you..?">
+                        </form>
+                        <a href="javascript: void(0);" class="sidebar-icons">
+                            <img src="{{asset('assets/images/figma/question.png')}}" height="15" width="15" alt="">
+                        </a>
+                        <a href="javascript: void(0);" class="sidebar-icons">
+                            <img src="{{asset('assets/images/figma/settings.png')}}" height="15" width="15" alt="">
+                        </a>
+                    </div>
+                    <div class="d-flex h-50-px align-items-center justify-content-between border-bottom-1 px-3">
+                        <span class="fs-12 fw-600">Ghostship</span>
+                        <span class="fs-10">...Thinking</span>
+                    </div>
+                    <div class="d-flex h-50-px align-items-center justify-content-between border-bottom-1 px-3">
+                        <span class="fs-14 fw-600">Recent Conversations</span>
+                    </div>
+                </div>
+                <div class="container-fluid">
+
+                    <div class="conversation">
+                        <div class="d-flex conversation-section">
+                    <span>
+                      <img src="{{asset('assets/images/figma/Vector.png')}}" class="me-2" height="15" width="15" alt="">
+                    </span>
+                            <span>What shoes are releasing this month?</span>
+                        </div>
+                        <div class="d-flex conversation-section">
+                    <span>
+                      <img src="{{asset('assets/images/figma/Vector.png')}}" class="me-2" height="15" width="15" alt="">
+                    </span>
+                            <span>Can you let me know what raffles are open?</span>
+                        </div>
+                        <div class="d-flex conversation-section">
+                    <span>
+                      <img src="{{asset('assets/images/figma/Vector.png')}}" class="me-2" height="15" width="15" alt="">
+                    </span>
+                            <span>What notifications did I miss in the last hour?</span>
+                        </div>
+                        <div class=" conversation-section mb-3">
+                            <a href="javascript: void(0);" class="d-flex align-items-center">
+                                <i class="ri-add-line me-1 fs-14"></i>
+                                <span class="fs-15 text-black fw-500">More</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="container-fluid">
+                    <div class="conversation">
+                        <div class="d-flex conversation-section">
+                    <span>
+                      <img src="{{asset('assets/images/figma/discord.png')}}" class="me-2" height="22" width="22"
+                           alt="">
+                    </span>
+                            <span class="text-uppercase text-black fw-bold">Commands</span>
+                        </div>
+                        <div class="d-flex conversation-section">
+                    <span>
+                      <img src="{{asset('assets/images/figma/hash.png')}}" class="me-2" height="12" width="12" alt="">
+                    </span>
+                            <span>Release calendar</span>
+                        </div>
+                        <div class="d-flex conversation-section">
+                    <span>
+                      <img src="{{asset('assets/images/figma/hash.png')}}" class="me-2" height="15" width="15" alt="">
+                    </span>
+                            <span>Login</span>
+                        </div>
+                        <div class="d-flex conversation-section">
+                    <span>
+                      <img src="{{asset('assets/images/figma/hash.png')}}" class="me-2" height="15" width="15" alt="">
+                    </span>
+                            <span>Raffles</span>
+                        </div>
+                        <div class="d-flex conversation-section">
+                    <span>
+                      <img src="{{asset('assets/images/figma/hash.png')}}" class="me-2" height="15" width="15" alt="">
+                    </span>
+                            <span>News</span>
+                        </div>
+                        <div class="d-flex conversation-section">
+                    <span>
+                      <img src="{{asset('assets/images/figma/hash.png')}}" class="me-2" height="15" width="15" alt="">
+                    </span>
+                            <span>Tweet</span>
+                        </div>
+                        <div class="d-flex conversation-section">
+                    <span>
+                      <img src="{{asset('assets/images/figma/hash.png')}}" class="me-2" height="15" width="15" alt="">
+                    </span>
+                            <span>Checkout</span>
+                        </div>
+                        <div class=" conversation-section mb-3">
+                            <a href="javascript: void(0);" class="d-flex align-items-center">
+                                <i class="ri-add-line me-1 fs-14"></i>
+                                <span class="fs-15 text-black fw-500">More</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="container-fluid">
+                    <div class="conversation">
+                        <div class="d-flex conversation-section">
+                    <span>
+                      <img src="{{asset('assets/images/figma/people.svg')}}" class="me-2" height="22" width="22" alt="">
+                    </span>
+                            <span class="text-uppercase text-black fw-bold">COMMUNITY</span>
+                        </div>
+                        <a href="javascript: void(0);" class="d-flex conversation-section mb-5">
+                    <span>
+                      <img src="{{asset('assets/images/figma/badge.png')}}" class="me-2" height="18" width="18" alt="">
+                    </span>
+                            <span>Upgrade to Pro</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="container-fluid">
+                    <div>
+                        <div class="d-flex conversation-section align-items-end p-4">
+                    <span>
+                      <img src="{{asset('assets/images/figma/ghostship.png')}}" class="me-2" height="68" width="60"
+                           alt="">
+                    </span>
+                            <div class="notifications-alert">
+                                <p class="fs-11">
+                                    When do you want to see the notifications?
+                                </p>
+                                <div class="d-flex">
+                                    <button class="rounded-pill btn btn-sm btn-outline-secondary waves-effect waves-light me-1">
+                                        Hour
+                                    </button>
+                                    <button class="btn btn-secondary btn-sm rounded-pill waves-effect waves-light me-1">
+                                        Daily
+                                    </button>
+                                    <button class="rounded-pill btn btn-sm btn-outline-secondary waves-effect waves-light">
+                                        Minutes
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </ul>
+        </div>
+        <!-- Sidebar -->
+    </div>
+
+    <div class="sidebar-background"></div>
+</div>

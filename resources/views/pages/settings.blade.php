9dd721acf2f3c9bdef6994269a5adc8a15ead67a
Iamqasimmansoor <114659842+Iamqasimmansoor@users.noreply.github.com>
2023-03-02 16:17:46 +0500

    Settings screen responsiveness fixed
    Settings screen responsiveness fixed

--- resources/views/pages/settings.blade.php
+++ resources/views/pages/settings.blade.php
@@ -78,6 +78,11 @@
         <div class="page-content" style="padding:0;padding-top:calc(36px + 1.5rem) !important;">
            <div class="settings-container">
                 <div class="setting-navigation">
+                    <div class="text-end mb-2 nav-toggle-close-box">
+                        <a href="javascript:void(0)" class="navigation-toggle-close fs-18">
+                            <i class="mdi mdi-close"></i>
+                        </a>
+                    </div>
                     <ul class="nav" id="myTab" role="tablist">
                         <li class="nav-item">
                             <a href="#general-setting" class="nav-link active" data-bs-toggle="tab" role="tab">General Settings</a>
@@ -112,6 +117,9 @@
                     <div class="tab-content" id="myTabContent">
                         <div class="tab-pane fade show active" id="general-setting" role="tabpanel">
                             <div class="setting-header">
+                                <a href="javascript:void(0)" class="navigation-toggle-btn fs-18">
+                                    <i class="mdi mdi-menu"></i>
+                                </a>
                                 <h2 class="mb-0">General Settings</h2>
                             </div>
                             <div class="setting-content-body">
@@ -132,7 +140,9 @@
                                 <div class="setting-item">
                                     <div class="d-flex align-items-center justify-content-between gap-2 mb-1">
                                         <p class="mb-0 fw-600 fs-14">Accounts connected (authorized apps)</p>
-                                        <a href="javascript:void(0)" class="btn btn-light text-dark btn-custom">Add</a>
+                                        <div class="form-check form-switch me-3">
+                                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                                        </div>
                                     </div>
                                     <p class="mb-0 fs-13 text-theme-light-grey">Massa arcu donec donec scelerisque phasellus tellus sed risus.</p>
                                 </div>
@@ -150,70 +160,131 @@
                                     </div>
                                     <p class="mb-0 fs-13 text-theme-light-grey">Nunc elit semper diam risus fames. Vulputate cras tempus id nulla.</p>
                                 </div>
+                                <div class="setting-item">
+                                    <div class="d-flex align-items-center justify-content-between gap-2 mb-1">
+                                        <p class="mb-0 fw-600 fs-14">Two-factor authentication</p>
+                                        <div class="form-check form-switch me-3">
+                                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                                        </div>
+                                    </div>
+                                    <p class="mb-0 fs-13 text-theme-light-grey">Tempor arcu massa dapibus consequat et aliquam morbi sit. Quam.</p>
+                                </div>
+                                <div class="setting-item">
+                                    <div class="d-flex align-items-center justify-content-between gap-2 mb-1">
+                                        <p class="mb-0 fw-600 fs-14">Billing</p>
+                                    </div>
+                                    <p class="mb-0 fs-13 text-theme-light-grey">Cras vitae eu in vitae nunc sit. Mauris sapien eu quam cras vel.</p>
+                                </div>
+                                <div class="setting-item">
+                                    <div class="d-flex align-items-center justify-content-between gap-2 mb-1">
+                                        <p class="mb-0 fw-600 fs-14">Signout</p>
+                                    </div>
+                                    <p class="mb-0 fs-13 text-theme-light-grey">Est augue ornare in leo. Duis ultricies commodo lobortis vitae.</p>
+                                </div>
                             </div>
                         </div>
                         <div class="tab-pane fade" id="example1" role="tabpanel">
                             <div class="setting-header">
+                                <a href="javascript:void(0)" class="navigation-toggle-btn fs-18">
+                                    <i class="mdi mdi-menu"></i>
+                                </a>
                                 <h2 class="mb-0">Example 1</h2>
                             </div>
                             <div class="setting-content-body">
-                                <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo obcaecati ut pariatur distinctio quidem soluta nam natus mollitia odit asperiores molestiae, quod qui ullam odio quas explicabo accusantium maxime veritatis.</p>
+                                <div class="setting-item">
+                                    <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo obcaecati ut pariatur distinctio quidem soluta nam natus mollitia odit asperiores molestiae, quod qui ullam odio quas explicabo accusantium maxime veritatis.</p>
+                                </div>
                             </div>
                         </div>
                         <div class="tab-pane fade" id="example2" role="tabpanel">
                             <div class="setting-header">
+                                <a href="javascript:void(0)" class="navigation-toggle-btn fs-18">
+                                    <i class="mdi mdi-menu"></i>
+                                </a>
                                 <h2 class="mb-0">Example 2</h2>
                             </div>
                             <div class="setting-content-body">
-                                <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo obcaecati ut pariatur distinctio quidem soluta nam natus mollitia odit asperiores molestiae, quod qui ullam odio quas explicabo accusantium maxime veritatis.</p>
+                                <div class="setting-item">
+                                    <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo obcaecati ut pariatur distinctio quidem soluta nam natus mollitia odit asperiores molestiae, quod qui ullam odio quas explicabo accusantium maxime veritatis.</p>
+                                </div>
                             </div>
                         </div>
                         <div class="tab-pane fade" id="example3" role="tabpanel">
                             <div class="setting-header">
+                                <a href="javascript:void(0)" class="navigation-toggle-btn fs-18">
+                                    <i class="mdi mdi-menu"></i>
+                                </a>
                                 <h2 class="mb-0">Example 3</h2>
                             </div>
                             <div class="setting-content-body">
-                                <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo obcaecati ut pariatur distinctio quidem soluta nam natus mollitia odit asperiores molestiae, quod qui ullam odio quas explicabo accusantium maxime veritatis.</p>
+                                <div class="setting-item">
+                                    <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo obcaecati ut pariatur distinctio quidem soluta nam natus mollitia odit asperiores molestiae, quod qui ullam odio quas explicabo accusantium maxime veritatis.</p>
+                                </div>
                             </div>
                         </div>
                         <div class="tab-pane fade" id="example4" role="tabpanel">
                             <div class="setting-header">
+                                <a href="javascript:void(0)" class="navigation-toggle-btn fs-18">
+                                    <i class="mdi mdi-menu"></i>
+                                </a>
                                 <h2 class="mb-0">Example 4</h2>
                             </div>
                             <div class="setting-content-body">
-                                <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo obcaecati ut pariatur distinctio quidem soluta nam natus mollitia odit asperiores molestiae, quod qui ullam odio quas explicabo accusantium maxime veritatis.</p>
+                                <div class="setting-item">
+                                    <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo obcaecati ut pariatur distinctio quidem soluta nam natus mollitia odit asperiores molestiae, quod qui ullam odio quas explicabo accusantium maxime veritatis.</p>
+                                </div>
                             </div>
                         </div>
                         <div class="tab-pane fade" id="example5" role="tabpanel">
                             <div class="setting-header">
+                                <a href="javascript:void(0)" class="navigation-toggle-btn fs-18">
+                                    <i class="mdi mdi-menu"></i>
+                                </a>
                                 <h2 class="mb-0">Example 5</h2>
                             </div>
                             <div class="setting-content-body">
-                                <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo obcaecati ut pariatur distinctio quidem soluta nam natus mollitia odit asperiores molestiae, quod qui ullam odio quas explicabo accusantium maxime veritatis.</p>
+                                <div class="setting-item">
+                                    <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo obcaecati ut pariatur distinctio quidem soluta nam natus mollitia odit asperiores molestiae, quod qui ullam odio quas explicabo accusantium maxime veritatis.</p>
+                                </div>
                             </div>
                         </div>
                         <div class="tab-pane fade" id="example6" role="tabpanel">
                             <div class="setting-header">
+                                <a href="javascript:void(0)" class="navigation-toggle-btn fs-18">
+                                    <i class="mdi mdi-menu"></i>
+                                </a>
                                 <h2 class="mb-0">Example 6</h2>
                             </div>
                             <div class="setting-content-body">
-                                <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo obcaecati ut pariatur distinctio quidem soluta nam natus mollitia odit asperiores molestiae, quod qui ullam odio quas explicabo accusantium maxime veritatis.</p>
+                                <div class="setting-item">
+                                    <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo obcaecati ut pariatur distinctio quidem soluta nam natus mollitia odit asperiores molestiae, quod qui ullam odio quas explicabo accusantium maxime veritatis.</p>
+                                </div>
                             </div>
                         </div>
                         <div class="tab-pane fade" id="example7" role="tabpanel">
                             <div class="setting-header">
+                                <a href="javascript:void(0)" class="navigation-toggle-btn fs-18">
+                                    <i class="mdi mdi-menu"></i>
+                                </a>
                                 <h2 class="mb-0">Example 7</h2>
                             </div>
                             <div class="setting-content-body">
-                                <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo obcaecati ut pariatur distinctio quidem soluta nam natus mollitia odit asperiores molestiae, quod qui ullam odio quas explicabo accusantium maxime veritatis.</p>
+                                <div class="setting-item">
+                                    <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo obcaecati ut pariatur distinctio quidem soluta nam natus mollitia odit asperiores molestiae, quod qui ullam odio quas explicabo accusantium maxime veritatis.</p>
+                                </div>
                             </div>
                         </div>
                         <div class="tab-pane fade" id="example8" role="tabpanel">
                             <div class="setting-header">
+                                <a href="javascript:void(0)" class="navigation-toggle-btn fs-18">
+                                    <i class="mdi mdi-menu"></i>
+                                </a>
                                 <h2 class="mb-0">Example 8</h2>
                             </div>
                             <div class="setting-content-body">
-                                <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo obcaecati ut pariatur distinctio quidem soluta nam natus mollitia odit asperiores molestiae, quod qui ullam odio quas explicabo accusantium maxime veritatis.</p>
+                                <div class="setting-item">
+                                    <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo obcaecati ut pariatur distinctio quidem soluta nam natus mollitia odit asperiores molestiae, quod qui ullam odio quas explicabo accusantium maxime veritatis.</p>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -221,4 +292,20 @@
            </div>
         </div>
     </div>
+@endsection
+
+@section('custom-scripts')
+<script>
+    $('.navigation-toggle-btn').on('click',function(){
+        $('.setting-navigation').fadeIn();
+    });
+    $('.navigation-toggle-close').on('click',function(){
+        $('.setting-navigation').fadeOut();
+    });
+    $('#myTab li').on('click',function(){
+        if($(window).width() <= 768){
+            $('.setting-navigation').fadeOut();
+        } 
+    });
+</script>
 @endsection

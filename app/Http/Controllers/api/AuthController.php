7848a9061ee80085ab74b529a7ffd95c004cb589
Iamqasimmansoor <qasimmansoor683@gmail.com>
2023-04-03 15:58:34 +0500

    Authentication API fixed
    Authentication API fixed

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -43,7 +43,7 @@ class AuthController extends Controller
             $data['otp'] = $otp;
             $data = json_encode($data);
             $information = [
-                'email' => $register->email,
+                'email' => $user->email,
                 'subject' => 'Email verification code'
             ];
             $information = json_encode($information);
